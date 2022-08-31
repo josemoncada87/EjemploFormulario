978ddbafa6ee2bf41578f42ce139b57b1b7091ca
josemoncada87 <josemoncada87@hotmail.com>
2022-08-31 11:47:56 -0500

    second with js validation :D

--- main.js
+++ main.js
@@ -7,11 +7,44 @@ let listAmigos = document.querySelector("#list");
 //console.log(numEdad);
 //console.log(dateCumple);
 btnCrear.addEventListener("click", (ev)=>{
-    ev.preventDefault();
-    console.log(txtNombre.value); 
-    console.log(numEdad.value); 
-    console.log(dateCumple.value); 
-    let nuevoNombre = document.createElement("li");
-    nuevoNombre.textContent = txtNombre.value + ", " +  numEdad.value +", "+dateCumple.value;
-    listAmigos.appendChild(nuevoNombre);
+    ev.preventDefault();    
+    if( txtNombre.value!="" && numEdad.value!="" && dateCumple.value!="" ){
+        let nuevoNombre = document.createElement("li");
+        nuevoNombre.textContent = txtNombre.value + ", " +  numEdad.value +", "+dateCumple.value;
+        listAmigos.appendChild(nuevoNombre);
+        txtNombre.value="";
+        numEdad.value="";
+        dateCumple.value="";
+    }
+
+    if(txtNombre.value==""){
+        let errorMsg = document.querySelector("#error-name");
+        errorMsg.classList.add("active");
+        errorMsg.classList.remove("inactive");
+    }else{
+        let errorMsg = document.querySelector("#error-name");        
+        errorMsg.classList.add("inactive");
+        errorMsg.classList.remove("active");
+    }
+
+    if(numEdad.value==""){
+        let errorMsg = document.querySelector("#error-age");
+        errorMsg.classList.add("active");
+        errorMsg.classList.remove("inactive");
+    }else{
+        let errorMsg = document.querySelector("#error-age");        
+        errorMsg.classList.add("inactive");
+        errorMsg.classList.remove("active");
+    }
+
+    if(dateCumple.value==""){
+        let errorMsg = document.querySelector("#error-birt");
+        errorMsg.classList.add("active");
+        errorMsg.classList.remove("inactive");
+    }else{
+        let errorMsg = document.querySelector("#error-birt");        
+        errorMsg.classList.add("inactive");
+        errorMsg.classList.remove("active");
+    }
+
 });
